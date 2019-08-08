130885fd2dbb7301a3cd267fe83e02d88a9dc738
liuqikang <liuqikang@jd.com>
2019-08-08 16:47:14 +0800

    摄像头显示

--- app/src/main/java/com/example/liuqikang/myapplication/camera/CameraActivity.java
+++ app/src/main/java/com/example/liuqikang/myapplication/camera/CameraActivity.java
@@ -13,6 +13,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -20,6 +21,8 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,7 +40,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CameraActivity extends Activity implements SurfaceHolder.Callback,
-        SurfaceTexture.OnFrameAvailableListener {
+        SurfaceTexture.OnFrameAvailableListener, View.OnClickListener {
     private static final String TAG = CameraActivity.class.getName();
 
     private static final int VIDEO_WIDTH = 1920;
@@ -57,6 +60,16 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback,
 
     private TextView fpsText;
     private Spinner fpsSpinner;
+
+    private FrameLayout frameLayout;
+
+    private Button frameMove;
+    private Button surfaceMove;
+    private Button frameRotation;
+    private Button frameScale;
+
+    boolean canSurfaceMove = false;
+    boolean canFrameMove = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -90,6 +103,16 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback,
             }
         });
 
+        frameLayout = (FrameLayout) findViewById(R.id.camera_frame_layout);
+        frameMove = (Button) findViewById(R.id.camera_framelayout_move);
+        surfaceMove = (Button) findViewById(R.id.camera_surfaceView_move);
+        frameRotation = (Button)findViewById(R.id.camera_framelayout_rotation);
+        frameScale = (Button) findViewById(R.id.camera_framelayout_scale);
+        frameMove.setOnClickListener(this);
+        surfaceMove.setOnClickListener(this);
+        frameRotation.setOnClickListener(this);
+        frameScale.setOnClickListener(this);
+
         surfaceView = (SurfaceView) findViewById(R.id.camera_surfaceview);
         SurfaceHolder holder = surfaceView.getHolder();
         holder.addCallback(this);
@@ -102,7 +125,7 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback,
                 PropertyValuesHolder valuesHolder = PropertyValuesHolder.ofFloat("rotationY", 0.0f, 360.0f, 0.0F);
                 PropertyValuesHolder valuesHolder1 = PropertyValuesHolder.ofFloat("scaleX", 1.0f, 0.5f,1.0f);
                 PropertyValuesHolder valuesHolder3 = PropertyValuesHolder.ofFloat("scaleY", 1.0f, 0.5f,1.0f);
-                ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(surfaceView,  valuesHolder,valuesHolder1,valuesHolder3);
+                ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(frameLayout,  valuesHolder,valuesHolder1,valuesHolder3);
                 objectAnimator.setDuration(5000).start();
             }
         });
@@ -322,6 +345,51 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback,
         mHandler.sendEmptyMessage(CameraHandler.MSG_FRAME_AVAILABLE);
     }
 
+    int rotation = 90;
+    float scale = 0;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.camera_surfaceView_move:
+                canSurfaceMove = !canSurfaceMove;
+                surfaceMove.setText("surface移动:" + (canSurfaceMove ? "开" : "复位"));
+                if (!canSurfaceMove){
+                    surfaceView.setX(0);
+                    surfaceView.setY(0);
+                }
+                break;
+            case R.id.camera_framelayout_move:
+                canFrameMove = !canFrameMove;
+                frameMove.setText("frameLayout移动:" + (canFrameMove ? "开" : "复位"));
+                if (!canFrameMove){
+                    frameLayout.setX(0);
+                    frameLayout.setY(0);
+                }
+                break;
+            case R.id.camera_framelayout_rotation:
+                frameLayout.setRotation(rotation);
+                if (rotation == 90){
+                    rotation = 0;
+                    frameRotation.setText("旋转复位");
+                }else {
+                    rotation = 90;
+                    frameRotation.setText("旋转90");
+                }
+                break;
+            case R.id.camera_framelayout_scale:
+                frameLayout.setScaleX(scale);
+                frameLayout.setScaleY(scale);
+                if (scale == 0.5){
+                    scale = 1;
+                    frameScale.setText("缩放复位");
+                }else {
+                    scale = 0.5f;
+                    frameScale.setText("缩放0.5");
+                }
+                break;
+        }
+    }
+
     private static class CameraHandler extends Handler{
         // 刷新视频帧
         public static final int MSG_FRAME_AVAILABLE = 0;
@@ -361,5 +429,35 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback,
         } else {
             openCamera(VIDEO_WIDTH, VIDEO_HEIGHT, DESIRED_PREVIEW_FPS);
         }
+    }
+
+    float surfaceTouchX;
+    float surfaceTouchY;
+
+    float framelayoutTouchX;
+    float framelayoutTouchY;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (canSurfaceMove){
+            if (event.getAction() == MotionEvent.ACTION_DOWN){
+                surfaceTouchX = event.getX() - surfaceView.getX();
+                surfaceTouchY = event.getY() - surfaceView.getY();
+            }
+
+            surfaceView.setX(event.getX() - surfaceTouchX);
+            surfaceView.setY(event.getY() - surfaceTouchY);
+        }
+
+        if (canFrameMove){
+            if (event.getAction() == MotionEvent.ACTION_DOWN){
+                framelayoutTouchX = event.getX() - frameLayout.getX();
+                framelayoutTouchY = event.getY() - frameLayout.getY();
+            }
+
+            frameLayout.setX(event.getX() - framelayoutTouchX);
+            frameLayout.setY(event.getY() - framelayoutTouchY);
+        }
+
+        return super.onTouchEvent(event);
     }
 }
